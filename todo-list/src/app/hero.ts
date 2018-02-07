fccb726634b47e321e788ad9c44063d70b94bc02
Deacon <d.olesen53@gmail.com>
2018-02-07 16:05:35 -0500

    Master/Detail Componets start

--- todo-list/src/app/hero.ts
+++ todo-list/src/app/hero.ts
@@ -1,4 +1,6 @@
+// tslint:disable-next-line:one-line
 export class Hero{
     id: number;
     name: string;
+// tslint:disable-next-line:eofline
 }
---
+++ todo-list/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 100, name: 'The L-Man' },
+  { id: 99, name: 'Ashtray' },
+  { id: 98, name: 'Rides a 10 speed everywhere guy' },
+  { id: 97, name: 'Bicycle Repair Man' },
+  { id: 96, name: 'Jimbo Wales' },
+  { id: 95, name: 'Bouncing Boy' },
+  { id: 94, name: 'RubberMan' },
+  { id: 93, name: 'Maggott' },
+  { id: 92, name: 'Matter-Eater Lad' },
+  { id: 91, name: 'Magma' },
+  { id: 90, name: 'Tornado' }
+];
