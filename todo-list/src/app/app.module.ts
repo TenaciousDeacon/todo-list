3bf9db35a9feea4d91dcec4b81ad5162bbf8b485
Deacon <d.olesen53@gmail.com>
2018-02-07 13:59:53 -0500

    Starting Display a Heroes List

--- todo-list/src/app/app.module.ts
+++ todo-list/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms'; // ngModel lives there
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule// after adding import add FormsModule to imports array
   ],
   providers: [],
   bootstrap: [AppComponent]
