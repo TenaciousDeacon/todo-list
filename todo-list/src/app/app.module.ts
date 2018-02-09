45088fc3440402a25ae996256a9f45af020691ba
Deacon <d.olesen53@gmail.com>
2018-02-09 15:04:10 -0500

    Friday 2_9_18

--- todo-list/src/app/app.module.ts
+++ todo-list/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms'; // ngModel lives there
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // ngModel lives there
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule, FormsModule// after adding import add FormsModule to imports array
---
+++ todo-list/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+// This component only recieves a hero object thru its hero property and displays it
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
